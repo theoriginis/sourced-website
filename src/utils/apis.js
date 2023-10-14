b3426ee8676d2f8c1cd765007605b518eb99c85d
Originis <originiscode@gmail.com>
2023-10-14 22:20:20 +0530

    api corrected

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -68,7 +68,7 @@ export async function SearchPerformer(data) {
     return axios({
       method: "get",
       //url: `https://www.tn-apis.com/catalog/v2/events/search?websiteConfigId=${websiteConfigId}&q=*&performerFilter=text/name eq %27${performer_name}%27`,
-      url: `https://www.tn-apis.com/catalog/v2/performers/suggest?websiteConfigId=${websiteConfigId}&q=*&filter=contains(text%2Fname%2C '${performer_name}')`,
+      url: `https://www.tn-apis.com/catalog/v2/performers/suggest?websiteConfigId=${websiteConfigId}&q=*&filter=contains(text%2Fname%2C '${performer_name}')&filter=_metadata%2FticketCount%20ge%2010`,
       // params: {
       //   performerFilter: salesRankOptions,
       //          page: "1",
@@ -88,7 +88,7 @@ export async function SearchEventsByPerformer(data) {
     return axios({
       method: "get",
       //url: `https://www.tn-apis.com/catalog/v2/events/search?websiteConfigId=${websiteConfigId}&q=*&performerFilter=text/name eq %27${performer_name}%27`,
-      url: `https://www.tn-apis.com/catalog/v2/events/search?websiteConfigId=${websiteConfigId}&q=*&performerFilter=contains(text%2Fname%20eq%20'${performer_name}')&_metadata%2FticketCount%20ge%2010`,
+      url: `https://www.tn-apis.com/catalog/v2/events/search?websiteConfigId=${websiteConfigId}&q=*&filter=contains(text%2Fname%2C'${performer_name}')`,
       // params: {
       //   performerFilter: salesRankOptions,
       //          page: "1",
